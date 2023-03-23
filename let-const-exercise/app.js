0f24f5630fc61b79f0b503eca1281fd7999b3414
Britten Hanson <britten.hanson11@gmail.com>
2023-03-23 17:37:05 -0600

    add reduce exercises and exercises with mentor

---
+++ let-const-exercise/app.js
@@ -0,0 +1,15 @@
+//var PI = 3.14;
+//PI = 42; // stop me from doing this!
+
+const = 3.14;
+//now you cannot change because of the properties of const
+
+// - What is the difference between ***var*** and ***let***?
+// you can reassign and redeclare var, but let you can assign, but you cannot redeclare it
+//let is use in block scope 
+// - What is the difference between ***var*** and ***const***?
+// you cannot reassign or redeclare using const but you can with var. const creates block scope like let
+// - What is the difference between ***let*** and ***const***?
+// you can reassign with let but not with const - const stays constant
+// - What is hoisting?
+// how javascript lists or "hoists" variables to the tope of the scope they are declared in. So like var you can declare at the bottom of your js but it has scope in the whole thing so it will still work if you call it in a function before that
