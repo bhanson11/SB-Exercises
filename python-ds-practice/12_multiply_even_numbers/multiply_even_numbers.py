85c7fa19036aea0d3e40f1656daf8d40f83e4e4c
Britten Hanson <britten.hanson11@gmail.com>
2023-09-21 17:19:17 -0600

    create multiply_even_numbers function

--- python-ds-practice/12_multiply_even_numbers/multiply_even_numbers.py
+++ python-ds-practice/12_multiply_even_numbers/multiply_even_numbers.py
@@ -12,3 +12,11 @@ def multiply_even_numbers(nums):
         >>> multiply_even_numbers([1, 3, 5])
         1
     """
+
+    default = 1
+    
+    for num in nums: #for each number in the nums array
+        if num % 2 == 0: 
+            default = default * num
+    
+    return default 
