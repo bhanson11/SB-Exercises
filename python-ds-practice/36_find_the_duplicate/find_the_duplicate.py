655930818abe7816a4c4673c9df887376b2129e6
Britten Hanson <britten.hanson11@gmail.com>
2023-09-27 08:08:53 -0600

    create function to find duplicate number in list

--- python-ds-practice/36_find_the_duplicate/find_the_duplicate.py
+++ python-ds-practice/36_find_the_duplicate/find_the_duplicate.py
@@ -13,3 +13,10 @@ def find_the_duplicate(nums):
         >>> find_the_duplicate([2, 1, 3, 4]) is None
         True
     """
+
+    already_seen = set()
+
+    for num in nums: 
+        if num in already_seen:
+            return num
+        already_seen.add(num)
