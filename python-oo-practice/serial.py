de23ddc7fd12f57da73aa4b421f54573c9778098
Britten Hanson <britten.hanson11@gmail.com>
2023-10-10 15:27:11 -0600

    create serialgenerator for sequential numbers

--- python-oo-practice/serial.py
+++ python-oo-practice/serial.py
@@ -20,3 +20,19 @@ class SerialGenerator:
     100
     """
 
+    def __init__(self, start=0):
+        """Create a new generator starting at start or 0 as default"""
+        self.start = self.next = start
+    
+    def __repr__(self):
+        """Make a representation so it tells you start and next of serial generator"""
+        return f"<SerialGenerator start={self.start} next={self.next}"
+    
+    def generate(self):
+        """Generate the next sequential number"""
+        self.next = self.next + 1
+        return self.next - 1
+    
+    def reset(self):
+        """Reset to start"""
+        self.next = self.start
