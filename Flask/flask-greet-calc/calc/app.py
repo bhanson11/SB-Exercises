22158fc77be67dc5b284ad46d72090ed10f03187
Britten Hanson <britten.hanson11@gmail.com>
2023-10-24 19:56:12 -0600

    created math operations and functions in flask

--- Flask/flask-greet-calc/calc/app.py
+++ Flask/flask-greet-calc/calc/app.py
@@ -1,1 +1,37 @@
 # Put your app in here.
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/add')
+def add():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+
+    result = a + b
+    return result
+
+
+@app.route('/sub')
+def subtract():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+
+    result = a - b
+    return result
+
+@app.route('/mult')
+def multiply():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+
+    result = a * b
+    return result
+
+@app.route('/div')
+def divide():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+
+    result = a / b
+    return result
