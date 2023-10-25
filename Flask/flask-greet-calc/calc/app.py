313f99d41d4a5c482c9e76a54abb7829423dbf60
Britten Hanson <britten.hanson11@gmail.com>
2023-10-24 20:14:17 -0600

    change to take from the given operations.py for math operations

--- Flask/flask-greet-calc/calc/app.py
+++ Flask/flask-greet-calc/calc/app.py
@@ -1,37 +1,38 @@
 # Put your app in here.
 from flask import Flask
+from operations import add, sub, mult, div
 
 app = Flask(__name__)
 
 @app.route('/add')
-def add():
+def solve_add():
     a = int(request.args.get("a"))
     b = int(request.args.get("b"))
 
-    result = a + b
+    result = add(a,b)
     return result
 
 
 @app.route('/sub')
-def subtract():
+def solve_sub():
     a = int(request.args.get("a"))
     b = int(request.args.get("b"))
 
-    result = a - b
+    result = sub(a,b)
     return result
 
 @app.route('/mult')
-def multiply():
+def solve_mult():
     a = int(request.args.get("a"))
     b = int(request.args.get("b"))
 
-    result = a * b
+    result = mult(a,b)
     return result
 
 @app.route('/div')
-def divide():
+def solve_div():
     a = int(request.args.get("a"))
     b = int(request.args.get("b"))
 
-    result = a / b
+    result = div(a,b)
     return result
