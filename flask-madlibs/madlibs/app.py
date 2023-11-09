d49693078d8cd7410ea046dc521897103e43bb01
Britten Hanson <britten.hanson11@gmail.com>
2023-11-09 16:11:49 -0700

    ask prompts for fill in the blank madlib and then show resulting story

--- flask-madlibs/madlibs/app.py
+++ flask-madlibs/madlibs/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 from stories import story
 
@@ -13,7 +13,13 @@ app.config['SECRET_KEY'] = '<Madlibs>'
 toolbar = DebugToolbarExtension(app)
 
 @app.route("/")
-def fill_prompts():
+def ask_prompts():
     """Generate and show form to ask words."""
     prompts = story.prompts
     return render_template("questions.html", prompts=prompts)
+
+@app.route("/story")
+def show_story():
+    """show resulting story with inputs from user"""
+    text = story.generate(request.args)
+    return render_template("story.html", text=text)
