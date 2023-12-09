15687a2e7ca9222e18858ca5ebe11feaebe49b93
Britten Hanson <britten.hanson11@gmail.com>
2023-12-09 01:24:11 -0700

    import flask, add home page route, add index.html in templates and static folder

--- flask-boggle/app.py
+++ flask-boggle/app.py
@@ -1,3 +1,12 @@
+from flask import Flask
 from boggle import Boggle
 
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "BOGGLEROCKS"
+
 boggle_game = Boggle()
+
+@app.route("/")
+def home():
+    """Show boggle game board"""
+    return "Welcome to....BOGGLE!!"
