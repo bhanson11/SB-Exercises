f5c203279f5e16111a3308c87e690ab405b75a25
Britten Hanson <britten.hanson11@gmail.com>
2023-12-12 18:29:31 -0700

    create boggle.js and css fils

--- flask-boggle/app.py
+++ flask-boggle/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, jsonify, render_template, request, session
 from boggle import Boggle
 
 app = Flask(__name__)
@@ -10,16 +10,16 @@ boggle_game = Boggle()
 def home():
     """Show boggle game board"""
     board = boggle_game.make_board()
-    # session['board'] = board
-    # highscore = session.get("highscore", 0)
-    # num_plays = session.get("num_plays", 0)
+    session['board'] = board
 
     return render_template("index.html", board=board)
 
-@app.route("/verify-word")
-def verify_word():
+@app.route("/check-word-validity")
+def check_word():
     word = request.args["word"]
     board = session["board"]
     response = boggle_game.check_valid_word(board, word)
+
+    return jsonify([{'result': response}])
 
     
