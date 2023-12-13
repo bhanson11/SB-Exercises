00283d70acaa7cfe811aff2a388e39de53897b1a
Britten Hanson <britten.hanson11@gmail.com>
2023-12-12 17:59:21 -0700

    create board from the make board function in boggle.py, then work on verify if words are words

--- flask-boggle/app.py
+++ flask-boggle/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session
+from flask import Flask, render_template, request, session
 from boggle import Boggle
 
 app = Flask(__name__)
@@ -10,8 +10,16 @@ boggle_game = Boggle()
 def home():
     """Show boggle game board"""
     board = boggle_game.make_board()
-    session['board'] = board
-    highscore = session.get("highscore", 0)
-    num_plays = session.get("num_plays", 0)
+    # session['board'] = board
+    # highscore = session.get("highscore", 0)
+    # num_plays = session.get("num_plays", 0)
 
-    return render_template("index.html", board=board, highscore=highscore, num_plays=num_plays)
+    return render_template("index.html", board=board)
+
+@app.route("/verify-word")
+def verify_word():
+    word = request.args["word"]
+    board = session["board"]
+    response = boggle_game.check_valid_word(board, word)
+
+    
