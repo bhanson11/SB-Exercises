1d0cdbcc53419512c6b4eca6c8dd68204d5270ee
Britten Hanson <britten.hanson11@gmail.com>
2023-12-12 17:52:57 -0700

    make index.html wiht jinja for making board from the function in boggle.py

--- flask-boggle/app.py
+++ flask-boggle/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, session
 from boggle import Boggle
 
 app = Flask(__name__)
@@ -9,4 +9,9 @@ boggle_game = Boggle()
 @app.route("/")
 def home():
     """Show boggle game board"""
-    return "Welcome to....BOGGLE!!"
+    board = boggle_game.make_board()
+    session['board'] = board
+    highscore = session.get("highscore", 0)
+    num_plays = session.get("num_plays", 0)
+
+    return render_template("index.html", board=board, highscore=highscore, num_plays=num_plays)
