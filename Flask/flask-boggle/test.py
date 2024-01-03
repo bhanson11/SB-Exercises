910fc32cd1bf91fa09a43f89e3aa6c6703a5fc06
Britten Hanson <britten.hanson11@gmail.com>
2024-01-03 06:46:10 -0700

    setup Flask provided test client to allow for multiple tests to be run without making HTTP requests to running server

--- Flask/flask-boggle/test.py
+++ Flask/flask-boggle/test.py
@@ -7,4 +7,11 @@ from boggle import Boggle
 class FlaskTests(TestCase):
 
     # TODO -- write tests for every view function / feature!
+    def setUp(self):
+        """Stuff to do before every test in test.py for boggle"""
 
+        self.client = app.test_client()
+        app.config['TESTING'] = True
+    
+    # test the home page, check if homepage loads correctly, initializes session with game board and has right HTML and setup on page
+    
