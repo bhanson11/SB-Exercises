1e382aa0eb37fe945175c6259480999cc5777d06
Britten Hanson <britten.hanson11@gmail.com>
2024-01-22 10:13:26 -0700

    reupload starter coe for testing for this divide and conquer unit

--- data-structures-algorithms/prob-divide-conquer/count-zeroes.js
+++ data-structures-algorithms/prob-divide-conquer/count-zeroes.js
@@ -1,15 +1,5 @@
 function countZeroes() {
-    let count = 0;
-
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === 0) {
-            count++;
-        } else {
-            break;
-        }
-    }
-
-    return count;
+  
 }
 
 module.exports = countZeroes
