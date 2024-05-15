a307b5fd6224b5a1a4b76645ff8300970013d724
Britten Hanson <britten.hanson11@gmail.com>
2024-05-15 09:04:56 -0600

    add Posts Model

--- SQL/flask-blogly/models.py
+++ SQL/flask-blogly/models.py
@@ -1,3 +1,4 @@
+import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -15,9 +16,18 @@ class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    
     first_name = db.Column(db.Text, nullable=False)
-
     last_name = db.Column(db.Text, nullable=False)
-
     image_url = db.Column(db.Text, nullable=True, default=DEFAULT_IMAGE_URL)
+
+class Post(db.Model):
+    """User posts"""
+
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
