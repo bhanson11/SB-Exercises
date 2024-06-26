1678c4c1a3d4ae80e7462fa52e8ca5a6b0461291
Britten Hanson <britten.hanson11@gmail.com>
2024-06-25 18:10:31 -0600

    set up route for viewing all cupcakes and serializing to view in JSON

--- Flask/flask-cupcakes/app.py
+++ Flask/flask-cupcakes/app.py
@@ -1,1 +1,20 @@
 """Flask app for Cupcakes"""
+from flask import Flask, request, jsonify
+
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+app.app_context().push()
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = "secrecy"
+
+connect_db(app)
+
+@app.route('/api/cupcakes', methods=['GET'])
+def list_cupcakes():
+    """Get data about all cupcakes and respond with JSON"""
+    all_cupcakes = [cupcake.serialize() for cupcake in Cupcake.query.all()]
+    return jsonify(cupcakes=all_cupcakes)
+
